70c7e42e367b7c70c460b7e74aebe52c5fe2c40e
Chris Lehneis <cdlehneis@gmail.com>
2016-07-22 00:53:04 -0400

    adds some fonts

--- src/app/components/about/About.jsx
+++ src/app/components/about/About.jsx
@@ -4,6 +4,15 @@ function About() {
   return (
     <div className="container about">
       <h1>About</h1>
+      <p>
+      	This is the about page, where we talk about how awesome our app is. Maybe you think we're cool too. ;)
+      </p>
+      <p>
+      	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius eos eligendi inventore. Quisquam molestias, adipisci optio suscipit. Et reiciendis quibusdam, deserunt ad. Dolorem aperiam, molestias architecto est aliquam voluptatem quidem!
+      </p>
+      <p>
+      	This layout will change for sure.
+      </p>
     </div>
   )
 }
