f9e4ddc86dcafbbb5a30e85723dda8d60ad6650a
Jason Sohl <jason@frozensoliddesigns.com>
2016-07-22 16:00:17 -0700

    basic socket.io setup

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('./'));
 app.use(express.static('dist'));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/dist/index.html`);
+});
+
+app.get('/send-alert', (req, res) => {
+  res.sendFile(`${__dirname}/src/index.html`);
+  setTimeout(function(){
+    io.emit('some_event', { for: 'everyone' });
+  },5000);
+});
+
+io.on('connection', function(socket){
+
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log('app listening on', port);
 });
--- src/app/components/about/About.jsx
+++ src/app/components/about/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <div className="container about">
       <h1>About</h1>
       <p>
-      	This is the about page, where we talk about how awesome our app is. Maybe you think we're cool too. ;)
+      	This is the about, where we talk about how awesome our app is. Maybe you think we're cool too. ;)
       </p>
       <p>
       	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius eos eligendi inventore. Quisquam molestias, adipisci optio suscipit. Et reiciendis quibusdam, deserunt ad. Dolorem aperiam, molestias architecto est aliquam voluptatem quidem!
