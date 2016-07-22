2149ced9cca8e96fe17221920dd71d67cbbb6372
Jason Sohl <jason@frozensoliddesigns.com>
2016-07-22 14:05:55 -0700

    adding travisci testing

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef, no-unused-expressions */
 import { renderComponent, expect } from '../test_helper';
 import App from '../../src/app/components/App';
+import About from '../../src/app/components/about/About'
 
 describe('App', () => {
   let component;
@@ -10,6 +11,18 @@ describe('App', () => {
   });
 
   it('renders something', () => {
+    expect(component).to.exist;
+  });
+});
+
+describe('About', () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(About);
+  });
+
+  it('renders the about component', () => {
     expect(component).to.exist;
   });
 });
